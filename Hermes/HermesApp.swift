29f5bc4f3eb2d95e316566c6ca0eb8496c069eae
Mike Felder <2172656+mikefelder@users.noreply.github.com>
2026-08-06 19:38:24 -0500

    Initial Commit

---
+++ Hermes/HermesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HermesApp.swift
+//  Hermes
+//
+//  Created by Mike Felder on 8/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HermesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
